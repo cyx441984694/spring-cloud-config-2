d6e6f832d5dc9bf34a9c1dc1293eb89aa8fb881d
Winnie.Chen <37991693+cyx441984694@users.noreply.github.com>
2021-01-30 19:44:39 +0800

    Update HelloController.java

--- springcloudconfigclient/src/main/java/com/neo/web/HelloController.java
+++ springcloudconfigclient/src/main/java/com/neo/web/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 class HelloController {
     @Value("${neo.hello}")
     private String hello;
