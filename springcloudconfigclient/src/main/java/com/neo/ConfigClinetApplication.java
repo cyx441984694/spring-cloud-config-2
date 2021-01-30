442566ded65e2a75169f6bd305d207f41d7009f5
Winnie.Chen <37991693+cyx441984694@users.noreply.github.com>
2021-01-30 19:44:03 +0800

    Update ConfigClinetApplication.java

--- springcloudconfigclient/src/main/java/com/neo/ConfigClinetApplication.java
+++ springcloudconfigclient/src/main/java/com/neo/ConfigClinetApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 public class ConfigClinetApplication {
     public static void main(String[] args){
         SpringApplication.run(ConfigClinetApplication.class,args);
